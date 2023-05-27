4f0202e7d453ea00cc4f945e0301259d8b2e65fb
khadija <khadija@gmail.com>
2023-05-27 12:03:47 +0100

    Programs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import Programs from "./components/Programs/Programs";
 // import Hero from "./components/hero/Hero";
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Programs />
     </div>
   );
 }
