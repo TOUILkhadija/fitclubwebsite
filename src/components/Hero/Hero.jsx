cae7a66f9a7b88f28ce3978eedf4e03a7f7b4b32
khadija <khadija@gmail.com>
2023-05-31 18:35:33 +0100

    footer section + blur

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import './Hero.css'
-import Header from '../Header/Header'
-import hero_image from '../../assets/hero_image.png'
-import hero_image_back from "../../assets/hero_image_back.png"
-import Heart from "../../assets/heart.png"
-import Calories from "../../assets/calories.png"
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 const Hero = () => {
   return (
     <div className="hero">
+      <div className="blur hero-blur"></div>
       <div className="left-h">
         <Header />
         {/*the best ad */}
         <div className="the-best-ad">
-          <div>
-          </div>
+          <div></div>
           <span>the best fitness club in the town</span>
         </div>
         <div className="hero-text">
           <div>
-            <span className='stroke-text' >
-              Shape </span>
+            <span className="stroke-text">Shape </span>
             <span> Your</span>
           </div>
-          <div><span>
-            Ideal body</span></div>
           <div>
-            In here we will help you to shape and build your ideal body and live up your life to fillest
+            <span>Ideal body</span>
+          </div>
+          <div>
+            In here we will help you to shape and build your ideal body and live
+            up your life to fillest
             <span></span>
           </div>
         </div>
@@ -49,31 +50,29 @@ const Hero = () => {
         <div className="hero-buttons">
           <buttons className="btn">Get started</buttons>
           <buttons className="btn">Learn More</buttons>
-
         </div>
       </div>
       <div className="right-h">
         <button className="btn">Join Now</button>
         <div className="heart-rate">
           <img src={Heart} alt="" />
-          <span>Heart Rate</span><span>116 bpm</span>
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
         </div>
         {/* hero images */}
         <img src={hero_image} alt="" className="hero-image" />
-        <img src={hero_image_back} alt="" className='hero-image-back' />
+        <img src={hero_image_back} alt="" className="hero-image-back" />
 
-        <div className='calories'>
+        <div className="calories">
           <img src={Calories} alt="" />
           <div>
             <span>Calories Burned</span>
             <span>220 kcal</span>
           </div>
         </div>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
